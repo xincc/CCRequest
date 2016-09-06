cc57bdd8a5ed6019ad17e0c4191a6ef31dff10bf
wangxin48 <wangxin48@baidu.com>
2016-09-06 10:40:53 +0800

    Fix ISSUE#2

--- HTTP/NetworkHelper/Request/CCRequestDispatchCenter.m
+++ HTTP/NetworkHelper/Request/CCRequestDispatchCenter.m
@@ -62,6 +62,9 @@
             manager.responseSerializer = [AFJSONResponseSerializer serializer];
             break;
         }
+        default: {
+            break;
+        }
     }
     
     NSMutableSet* acceptableContentTypeSet = [NSMutableSet setWithSet:manager.responseSerializer.acceptableContentTypes];
@@ -122,13 +125,23 @@
             break;
         }
         case CCRequestMethodPost: {
-            request.sessionTask = [manager POST:request.requestUrl parameters:request.requestArgument constructingBodyWithBlock:request.constructingBodyBlock progress:request.resumableUploadProgressBlock success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-                __strong __typeof(weakSelf) self = weakSelf;
-                [self handleRequestResult:task responseObject:responseObject error:nil];
-            } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                __strong __typeof(weakSelf) self = weakSelf;
-                [self handleRequestResult:task responseObject:nil error:error];
-            }];
+            if (!request.constructingBodyBlock) {
+                request.sessionTask = [manager POST:request.requestUrl parameters:request.requestArgument progress:request.resumableUploadProgressBlock success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                    __strong __typeof(weakSelf) self = weakSelf;
+                    [self handleRequestResult:task responseObject:responseObject error:nil];
+                } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                    __strong __typeof(weakSelf) self = weakSelf;
+                    [self handleRequestResult:task responseObject:nil error:error];
+                }];
+            } else {
+                request.sessionTask = [manager POST:request.requestUrl parameters:request.requestArgument constructingBodyWithBlock:request.constructingBodyBlock progress:request.resumableUploadProgressBlock success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                    __strong __typeof(weakSelf) self = weakSelf;
+                    [self handleRequestResult:task responseObject:responseObject error:nil];
+                } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                    __strong __typeof(weakSelf) self = weakSelf;
+                    [self handleRequestResult:task responseObject:nil error:error];
+                }];
+            }
             break;
         }
         case CCRequestMethodHead: {
